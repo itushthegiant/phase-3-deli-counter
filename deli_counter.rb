5cb61bc7b23511bb145b75d43495042a10759752
etamar <etamar.levy@gmail.com>
2021-07-22 21:55:57 -0400

    commit tree 1c5b32a925201fceb71ed6f1c6ce650dfc33e369

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def line(array)
+    if array.empty?
+        puts "The line is currently empty." 
+    else 
+        line_occupied = "The line is currently:"
+        array.map.with_index(1){|name, index| line_occupied << " #{index}. #{name}"}
+        puts line_occupied
+    end
+end
+
+
+def take_a_number(array, name)
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+
+def now_serving(array)
+    if array.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.shift()}."
+    end
+end
